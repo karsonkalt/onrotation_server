04f9843919b08447bb60069d426d248757136c16
Karson <kars.kalt@gmail.com>
2021-08-25 20:47:30 -0400

    Added todo note for Tracklist migration.

--- db/migrate/20210826004039_create_tracklists.rb
+++ db/migrate/20210826004039_create_tracklists.rb
@@ -5,6 +5,7 @@ class CreateTracklists < ActiveRecord::Migration[6.1]
       t.date :date_played
       t.references :artist, null: false, foreign_key: true
       t.string :soundcloud_track_id
+      # TODO Check the alias of creator to users running properly with db constraints
       t.references :creator, references: :users, null: false, foreign_key: { to_table: :users}
 
       t.timestamps
