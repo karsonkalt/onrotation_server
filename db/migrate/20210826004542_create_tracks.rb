255e9f49d9f320c8bdac6131079a5f635e00095c
Karson <kars.kalt@gmail.com>
2021-08-25 20:45:57 -0400

    Init Track model and migration.

---
+++ db/migrate/20210826004542_create_tracks.rb
@@ -0,0 +1,11 @@
+class CreateTracks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tracks do |t|
+      t.string :name
+      t.references :artist, null: false, foreign_key: true
+      t.references :label, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
