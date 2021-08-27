5fcc93440bb297f43a51dc83dc3cc86d19361d4d
Karson <kars.kalt@gmail.com>
2021-08-26 20:35:03 -0400

    UAliased suggested_track_identifications inside suggested_track_identification_votres because of 64 character limit in ActiveRecord.

--- db/migrate/20210826010722_create_suggested_track_identification_votes.rb
+++ db/migrate/20210826010722_create_suggested_track_identification_votes.rb
@@ -1,7 +1,7 @@
 class CreateSuggestedTrackIdentificationVotes < ActiveRecord::Migration[6.1]
   def change
     create_table :suggested_track_identification_votes do |t|
-      t.references :suggested_track_identification, null: false, foreign_key: true
+      t.references :identification, references: :suggested_track_identifications, null: false, foreign_key: { to_table: :suggested_track_identifications }
       # TODO Check the alias of voter to users running properly with db constraints
       t.references :voter, references: :users, null: false, foreign_key: { to_table: :users }
       t.boolean :is_identification_correct
