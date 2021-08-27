d76de93c121c274257bf6b8616142d2e733cbcb5
Karson <kars.kalt@gmail.com>
2021-08-26 22:30:07 -0400

    Change null constraint on BookmarkedTracklists table to fat arrow.

--- db/migrate/20210826011040_create_bookmarked_tracklists.rb
+++ db/migrate/20210826011040_create_bookmarked_tracklists.rb
@@ -1,8 +1,8 @@
 class CreateBookmarkedTracklists < ActiveRecord::Migration[6.1]
   def change
     create_table :bookmarked_tracklists do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :tracklist, null: false, foreign_key: true
+      t.references :user, :null => false, foreign_key: true
+      t.references :tracklist, :null => false, foreign_key: true
       t.boolean :has_unseen_updates
 
       t.timestamps
