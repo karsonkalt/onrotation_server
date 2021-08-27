dd70b72696197f26b4d54d0e6a7f8bf2a73918a9
Karson <kars.kalt@gmail.com>
2021-08-26 22:30:27 -0400

    Change null constraint on BookmarkedTracks table to fat arrow.

--- db/migrate/20210826011255_create_bookmarked_tracks.rb
+++ db/migrate/20210826011255_create_bookmarked_tracks.rb
@@ -1,8 +1,8 @@
 class CreateBookmarkedTracks < ActiveRecord::Migration[6.1]
   def change
     create_table :bookmarked_tracks do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :track, null: false, foreign_key: true
+      t.references :user, :null => false, foreign_key: true
+      t.references :track, :null => false, foreign_key: true
       t.boolean :has_unseen_updates
 
       t.timestamps
