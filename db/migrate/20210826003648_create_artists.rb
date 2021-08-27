f487170a3ce525680385349d945be6a88f1e94cc
Karson <kars.kalt@gmail.com>
2021-08-26 21:59:57 -0400

    Added fat arrow unique constraint to Artist migration and index artist name

--- db/migrate/20210826003648_create_artists.rb
+++ db/migrate/20210826003648_create_artists.rb
@@ -1,9 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[6.1]
   def change
     create_table :artists do |t|
-      t.string :name, unique: true
+      t.string :name, :unique => true
 
       t.timestamps
     end
+
+    add_index :artists, :name
   end
 end
