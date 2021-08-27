693fa983e380c4d8b5589323cb4913e1ff353221
Karson <kars.kalt@gmail.com>
2021-08-26 22:27:18 -0400

    Change uniqueness constraint on label table creator column to fat arrow.

--- db/migrate/20210826004501_create_labels.rb
+++ db/migrate/20210826004501_create_labels.rb
@@ -1,7 +1,7 @@
 class CreateLabels < ActiveRecord::Migration[6.1]
   def change
     create_table :labels do |t|
-      t.string :name, unique: true
+      t.string :name, :unique => true
 
       t.timestamps
     end
