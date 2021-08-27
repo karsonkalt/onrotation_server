e98b8f8b34b8d1924b9cb4d57684dcab1a3e2224
Karson <kars.kalt@gmail.com>
2021-08-26 21:01:28 -0400

    Seed generates random users with the Faker gem.

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# Generate random Users with Faker gem
+50.times do
+    if rand(2) == 1
+        first_name = Faker::Name.first_name
+        last_name = Faker::Name.last_name
+    end
+
+    if rand(5) == 1
+        uid = Faker::Alphanumeric.alphanumeric(number: 10)
+    end
+
+    User.create(
+        username: Faker::Internet.username,
+        first_name: first_name,
+        last_name: last_name,
+        email_address: Faker::Internet.free_email(name: first_name),
+        uid: uid
+    )
+end
