9bd84c76955e7233e90e653154334f4e57d6b822
Karson <kars.kalt@gmail.com>
2021-08-26 21:09:09 -0400

    Removed no null constraints from Track table.

--- db/migrate/20210826004542_create_tracks.rb
+++ db/migrate/20210826004542_create_tracks.rb
@@ -2,7 +2,7 @@ class CreateTracks < ActiveRecord::Migration[6.1]
   def change
     create_table :tracks do |t|
       t.string :name
-      t.references :artist, null: false, foreign_key: true
+      t.references :artist, foreign_key: true
       t.references :label, foreign_key: true
 
       t.timestamps
