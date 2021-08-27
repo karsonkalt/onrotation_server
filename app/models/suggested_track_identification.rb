3dbddc2f32a9702f834481466ab78b8450158492
Karson <kars.kalt@gmail.com>
2021-08-26 20:15:31 -0400

    Added has_many relationship to SuggestedTrackIdentification and added belongs_to identifier to model.

--- app/models/suggested_track_identification.rb
+++ app/models/suggested_track_identification.rb
@@ -1,4 +1,6 @@
 class SuggestedTrackIdentification < ApplicationRecord
   belongs_to :tracklist_track
   belongs_to :suggested_artist
+  belongs_to :identifier
+  has_many :suggested_track_identification_votes
 end
