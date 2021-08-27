2f1865f16c703390e8a4ad6aa72b9e2840213233
Karson <kars.kalt@gmail.com>
2021-08-26 22:26:15 -0400

    Change null and uniqueness constraints on tracklists table to fat arrow.

--- db/migrate/20210826004039_create_tracklists.rb
+++ db/migrate/20210826004039_create_tracklists.rb
@@ -3,8 +3,8 @@ class CreateTracklists < ActiveRecord::Migration[6.1]
     create_table :tracklists do |t|
       t.string :name
       t.date :date_played
-      t.references :artist, null: false, foreign_key: true
-      t.string :soundcloud_track_id, unique: true
+      t.references :artist, :null => false, foreign_key: true
+      t.string :soundcloud_track_id, :unique => true
       # TODO Check the alias of creator to users running properly with db constraints
       t.references :creator, references: :users, null: false, foreign_key: { to_table: :users}
 
