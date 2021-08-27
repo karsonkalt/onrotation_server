222349f1b1664d0999358945730829b31707769f
Karson <kars.kalt@gmail.com>
2021-08-26 21:04:44 -0400

    Added unique constraints to User table.

--- db/migrate/20210826003505_create_users.rb
+++ db/migrate/20210826003505_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, unique: true
       t.string :first_name
       t.string :last_name
-      t.string :email_address
-      t.string :uid
+      t.string :email_address, unique: true
+      t.string :uid, unique: true
 
       t.timestamps
     end
