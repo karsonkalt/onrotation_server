8abd6d43a5df33bc95f2624782364087e0bcb93e
Karson <kars.kalt@gmail.com>
2021-08-26 22:29:02 -0400

    Change null constraint on SuggestedTrackIdentifications table to fat arrow.

--- db/migrate/20210826010404_create_suggested_track_identifications.rb
+++ db/migrate/20210826010404_create_suggested_track_identifications.rb
@@ -1,12 +1,12 @@
 class CreateSuggestedTrackIdentifications < ActiveRecord::Migration[6.1]
   def change
     create_table :suggested_track_identifications do |t|
-      t.references :tracklist_track, null: false, foreign_key: true
+      t.references :tracklist_track, :null => false, foreign_key: true
       # TODO Check the alias of identifier to users running properly with db constraints
-      t.references :identifier, references: :users, null: false, foreign_key: { to_table: :users }
+      t.references :identifier, references: :users, :null => false, foreign_key: { to_table: :users }
       t.string :suggested_name
       # TODO Check the alias of suggested_artist to artist running properly with db constraints
-      t.references :suggested_artist, references: :artists, null: false, foreign_key: { to_table: :artists }
+      t.references :suggested_artist, references: :artists, :null => false, foreign_key: { to_table: :artists }
 
       t.timestamps
     end
