46bb9136b6adc5f0ae72e45783f6377a6a4201aa
Karson <kars.kalt@gmail.com>
2021-08-26 21:53:04 -0400

    Found issue with ActiveRecord not recognizing constraints, changed to fat arrow key value pair. Added indexes to common queried columns.

--- db/migrate/20210826003505_create_users.rb
+++ db/migrate/20210826003505_create_users.rb
@@ -1,13 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :username, unique: true
+      t.string :username, :null => false, :unique => true
       t.string :first_name
       t.string :last_name
-      t.string :email_address, unique: true
-      t.string :uid, unique: true
+      t.string :email_address, :null => false, :unique => true
+      t.string :uid
 
       t.timestamps
     end
+
+    add_index :users, :username
+    add_index :users, :email_address
   end
 end
