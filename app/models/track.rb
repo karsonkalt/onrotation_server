a5b5be5903bc70a22d279aac3f4c8259ef675b2f
Karson <kars.kalt@gmail.com>
2021-08-29 00:09:16 -0400

    Changed scoping names to be more descriptive. Added to-do comment.

--- app/models/track.rb
+++ app/models/track.rb
@@ -4,8 +4,10 @@ class Track < ApplicationRecord
   has_many :bookmarked_tracks
   has_many :tracklist_tracks
 
-  scope :unknown_name_and_artist, -> { where("name IS NULL and artist_id IS NULL") }
+  scope :unknown_name_unknown_artist, -> { where("name IS NULL and artist_id IS NULL") }
   scope :unknown_name_known_artist, -> { where("name IS NULL and artist_id IS NOT NULL") }
   scope :unreleased, -> { where("name IS NOT NULL and artist_id IS NOT NULL and label_id IS NULL") }
+
+  #TODO If ID'ing a track already in the database, then it should delete the record of the null track and update the tracklist_tracks to point to the track.
 
 end
