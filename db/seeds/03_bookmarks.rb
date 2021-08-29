b3dc8c1cd6de407fbb935a808e27053973ddf942
Karson <kars.kalt@gmail.com>
2021-08-29 08:08:54 -0400

    Add create_bookmarked_track method to call.

--- db/seeds/03_bookmarks.rb
+++ db/seeds/03_bookmarks.rb
@@ -1,10 +1,13 @@
 require_relative '02_tracklists'
 
-Users.all.each do |user|
-    rand(4).times do
-        BookmarkedTrack.create(
-            user: user,
-            track: Track.find(rand(Track.count) + 1)
-        )
+def create_bookmarked_tracks
+    Users.all.each do |user|
+        rand(4).times do
+            BookmarkedTrack.create(
+                user: user,
+                # TODO This should move to the Track model, and find_random_user
+                track: Track.find(rand(Track.count) + 1)
+            )
+        end
     end
 end
--- db/seeds/99_call.rb
+++ db/seeds/99_call.rb
@@ -2,3 +2,4 @@ require_relative '03_bookmarks'
 
 create_users($number_of_users)
 create_tracklists_tracks_artists_labels($tracklists, $number_of_users)
+create_bookmarked_tracks
