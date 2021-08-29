a42e8dc29e90e92c2c77a74d0eb535491822005c
Karson <kars.kalt@gmail.com>
2021-08-29 08:43:22 -0400

    Alias suggested_artist in model.

--- app/models/suggested_track_identification.rb
+++ app/models/suggested_track_identification.rb
@@ -1,6 +1,6 @@
 class SuggestedTrackIdentification < ApplicationRecord
   belongs_to :tracklist_track
-  belongs_to :suggested_artist
+  belongs_to :suggested_artist, :class_name => 'Artist'
   belongs_to :identifier, :class_name => 'User'
   has_many :suggested_track_identification_votes
 end
