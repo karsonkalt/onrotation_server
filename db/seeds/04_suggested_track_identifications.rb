d1aa3a1c948900ae506d678e08699c91867a35b9
Karson <kars.kalt@gmail.com>
2021-08-29 08:46:54 -0400

    create known artist unknown name suggested_track_identifications

--- db/seeds/04_suggested_track_identifications.rb
+++ db/seeds/04_suggested_track_identifications.rb
@@ -4,16 +4,32 @@ def create_suggested_track_identifications
     puts "Generating SuggestedTrackIdentifications\n"
 
     Track.unknown_name_unknown_artist.each do |track|
-        rand(2).times do
+        # TODO Change to rand() when scaling up
+        1.times do
 
-            byebug
-            
             SuggestedTrackIdentification.create(
                 # TODO Refactor this into User class as private method
                 identifier: User.find(rand(User.count) + 1),
                 # TODO Can track find its tracklist tracks?
                 tracklist_track: track.tracklist_tracks[0],
                 suggested_artist: Artist.find(rand(Artist.count) + 1),
+                suggested_name: Faker::Music::Phish.song
+            )
+
+            print "."
+        end
+    end
+
+    Track.unknown_name_known_artist.each do |track|
+        # TODO Change to rand() when scaling up
+        1.times do
+
+            SuggestedTrackIdentification.create(
+                # TODO Refactor this into User class as private method
+                identifier: User.find(rand(User.count) + 1),
+                # TODO Can track find its tracklist tracks?
+                tracklist_track: track.tracklist_tracks[0],
+                suggested_artist: track.artist,
                 suggested_name: Faker::Music::Phish.song
             )
 
