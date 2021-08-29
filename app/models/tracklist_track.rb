1606a6fba91fab8ca4e530f57e84ec5b9dc76ee8
Karson <kars.kalt@gmail.com>
2021-08-28 23:01:05 -0400

    Aliased identifier on TracklistTrack to User class.

--- app/models/tracklist_track.rb
+++ app/models/tracklist_track.rb
@@ -1,7 +1,7 @@
 class TracklistTrack < ApplicationRecord
   belongs_to :tracklist
   belongs_to :track
-  belongs_to :identifier
+  belongs_to :identifier, class: 'User'
   has_many :suggested_track_identifications
   # TODO Do I need to add the predessor has_one relationship?
 end
