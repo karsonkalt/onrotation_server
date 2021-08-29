d1d2edcfd1b092c854e3896ff9646bc11dfec80a
Karson <kars.kalt@gmail.com>
2021-08-28 22:51:47 -0400

    Aliased User as Creator in the model.

--- app/models/tracklist.rb
+++ app/models/tracklist.rb
@@ -1,6 +1,6 @@
 class Tracklist < ApplicationRecord
   belongs_to :artist
-  belongs_to :creator
+  belongs_to :creator, class_name: 'User'
   has_many :bookmarked_tracklists
   has_many :tracklist_tracks
 end
