d092f8a96a6fb67a502eb077bab3ad607f476ae8
Karson <kars.kalt@gmail.com>
2021-08-30 22:15:13 -0400

    Added alias to identification belongs to.

--- app/models/suggested_track_identification_vote.rb
+++ app/models/suggested_track_identification_vote.rb
@@ -1,4 +1,4 @@
 class SuggestedTrackIdentificationVote < ApplicationRecord
-  belongs_to :identification
+  belongs_to :identification, :class_name => 'SuggestedTrackIdentification'
   belongs_to :voter, :class_name => 'User'
 end
