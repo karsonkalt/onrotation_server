71f15181c47aa58ddca390536bbfcec0847843aa
Karson <kars.kalt@gmail.com>
2021-09-01 21:30:07 -0400

    Added TODO note about better way to handle linkedlist of TracklistTrack.

--- db/migrate/20210826005042_create_tracklist_tracks.rb
+++ db/migrate/20210826005042_create_tracklist_tracks.rb
@@ -5,6 +5,7 @@ class CreateTracklistTracks < ActiveRecord::Migration[6.1]
       t.references :track, :null => false, foreign_key: true
       t.time :cue_time
       # TODO Figure out the proper way to make predessor, either t.references and then self PK or t.int and enforce uniqueness.
+      # TODO A better way is to look at the next item, not the predecessor.
       t.integer :predessor_id
       # TODO Check the alias of identifier to users running properly with db constraints
       t.references :identifier, references: :users, :null => false, foreign_key: { to_table: :users }
