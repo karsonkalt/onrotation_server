215599088fa06c54d298a55079d1cce71757b6eb
Karson <kars.kalt@gmail.com>
2021-09-05 15:30:51 -0400

    Added alias for has_many relationship to suggested_track_identification_votes to model.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ApplicationRecord
     has_many :tracklists
     has_many :tracks
+    #TODO Add alias for has_many relationship to all models.
     has_many :suggested_track_identifications, :foreign_key => 'suggested_artist_id'
 end
--- app/models/suggested_track_identification.rb
+++ app/models/suggested_track_identification.rb
@@ -2,5 +2,5 @@ class SuggestedTrackIdentification < ApplicationRecord
   belongs_to :tracklist_track
   belongs_to :suggested_artist, :class_name => 'Artist'
   belongs_to :identifier, :class_name => 'User'
-  has_many :suggested_track_identification_votes
+  has_many :suggested_track_identification_votes, :foreign_key => 'identification_id'
 end
