3ab452fc9005da07f73485788e66ff659f1138a8
Karson <kars.kalt@gmail.com>
2021-09-17 19:13:21 -0400

    Added no-null constraint to Artist.

--- db/migrate/20210826003648_create_artists.rb
+++ db/migrate/20210826003648_create_artists.rb
@@ -1,7 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[6.1]
   def change
     create_table :artists do |t|
-      t.string :name, :unique => true
+      t.string :name, :unique => true, :null => false
 
       t.timestamps
     end
