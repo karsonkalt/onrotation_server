53b621af41c6df49f5555e9eaf5c90c6ab173a34
Karson <kars.kalt@gmail.com>
2021-09-17 19:23:16 -0400

    Added validation and constraints to suggested_track_identificaiton_votes.

--- app/models/suggested_track_identification_vote.rb
+++ app/models/suggested_track_identification_vote.rb
@@ -1,4 +1,6 @@
 class SuggestedTrackIdentificationVote < ApplicationRecord
   belongs_to :identification, :class_name => 'SuggestedTrackIdentification'
   belongs_to :voter, :class_name => 'User'
+
+  validates :is_identification_correct, presence: true
 end
--- db/migrate/20210826010722_create_suggested_track_identification_votes.rb
+++ db/migrate/20210826010722_create_suggested_track_identification_votes.rb
@@ -4,7 +4,7 @@ class CreateSuggestedTrackIdentificationVotes < ActiveRecord::Migration[6.1]
       t.references :identification, references: :suggested_track_identifications, :null => false, foreign_key: { to_table: :suggested_track_identifications }
       # TODO Check the alias of voter to users running properly with db constraints
       t.references :voter, references: :users, :null => false, foreign_key: { to_table: :users }
-      t.boolean :is_identification_correct
+      t.boolean :is_identification_correct, :null => false
 
       t.timestamps
     end
