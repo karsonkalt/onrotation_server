5b96096e0eefaa397a436d1707754fe86ed41e46
Karson <kars.kalt@gmail.com>
2021-09-24 09:57:44 -0700

    Abstracted IconText into component, changed style of search bar and notification button.

--- app/models/suggested_track_identification.rb
+++ app/models/suggested_track_identification.rb
@@ -7,8 +7,27 @@ class SuggestedTrackIdentification < ApplicationRecord
   #Globals to approve a suggested track identification
   @@percentage_to_be_approved = .75
   @@number_of_votes_to_be_approved = 4
+  @@percentage_to_be_rejected = .33
 
-  def vote
+  def vote(voter: voter, is_identification_correct: is_identification_correct)
+    SuggestedTrackIdentificationVote.create(identification: self, voter: voter, is_identification_correct: is_identification_correct)
+    votes = suggested_track_identification_votes
+    vote_count = votes.count
+
+    #TODO test next line
+    vote_count_correct = votes.select(is_identification_correct: true)
+    vote_percentage_correct = vote_count_correct/vote_count
+  
+    if vote_count >= @@number_of_votes_to_be_approved
+      if vote_percentage_correct >= @@percentage_to_be_approved
+        #destroy the vote records
+        suggested_track_identification_votes.each {|vote| vote.destroy}
+        #Turn it into an actual track
+      elsif vote_percentage_correct >= @@percentage_to_be_rejected
+        #throw away the suggestion
+      end
+    end
+
   end
 
 end
