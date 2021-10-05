573f4bd8cf5752e759b6c8a133af3248fd048411
Karson <kars.kalt@gmail.com>
2021-10-05 19:05:02 -0400

    Added note for N+1 queries.

--- app/controllers/tracklists_controller.rb
+++ app/controllers/tracklists_controller.rb
@@ -1,6 +1,7 @@
 class TracklistsController < ApplicationController
 
     def index
+        # Use .includes here? N+1 queries
         @tracklists = Tracklist.all
         render json: @tracklists, status: 200
     end
