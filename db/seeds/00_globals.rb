a2e4e5c7fd9e0f1666ea85bef35b891e8db4d39c
Karson <kars.kalt@gmail.com>
2021-10-08 17:56:59 -0400

    Updated seed file with more data.

--- db/seeds/00_globals.rb
+++ db/seeds/00_globals.rb
@@ -8,16 +8,16 @@ $tracklists = [
         youtube_url: 'https://www.youtube.com/watch?v=3H7YrkJEhqM&ab_channel=kalikande',
         tracks: [
             {name: 'Downriver', artist: 'Roger Martinez', label: 'Lost & Found', cue_time: "00:00:00"},
-            {name: 'Epika', artist: 'Guy Mantzur & Roy Rosenfeld', label: 'Kompakt', cue_time: "00:05:00"},
-            {name: 'Small Heart Attack (Guy J Remix)', artist: 'Guy Mantzur & Sahar Z', label: 'Lost & Found', cue_time: "00:08:00"},
-            {name: 'Matador On A Lose', artist: 'Guy J', label: nil, cue_time: "00:12:00"},
-            {name: 'Small Heart Attack (Agents of Time Reinterpretation', artist: 'Guy Mantzur & Sahar Z', label: 'Lost & Found', cue_time: "00:15:00"},
-            {name: 'Ego Tripping (Guy J Remix)', artist: 'Pedro Aguiar', label: 'Lost & Found', cue_time: "00:19:00"},
-            {name: nil, artist: nil, label: nil, cue_time: "00:21:00"},
-            {name: "Fool's Don't Last", artist: 'Guy J', label: 'Bedrock', cue_time: "00:29:00"},
-            {name: 'Modular Memories', artist: 'Blusoul', label: 'Lost & Found', cue_time: "00:30:00"},
-            {name: 'Vapourspace (Intro Mix)', artist: 'Sasha', label: 'Last Night On Earth', cue_time: "00:31:00"},
-            {name: 'Nirvana', artist: 'Guy J', label: 'Bedrock', cue_time: "00:35:00"}
+            {name: 'Epika', artist: 'Guy Mantzur & Roy Rosenfeld', label: 'Kompakt', cue_time: "00:05:40"},
+            {name: 'Small Heart Attack (Guy J Remix)', artist: 'Guy Mantzur & Sahar Z', label: 'Lost & Found', cue_time: "00:10:58"},
+            {name: 'Matador On A Lose', artist: 'Guy J', label: nil, cue_time: "00:16:45"},
+            {name: 'Small Heart Attack (Agents of Time Reinterpretation', artist: 'Guy Mantzur & Sahar Z', label: 'Lost & Found', cue_time: "00:21:25"},
+            {name: 'Ego Tripping (Guy J Remix)', artist: 'Pedro Aguiar', label: 'Lost & Found', cue_time: "00:29:25"},
+            {name: nil, artist: nil, label: nil, cue_time: "00:31:52"},
+            {name: "Fool's Don't Last", artist: 'Guy J', label: 'Bedrock', cue_time: "00:39:52"},
+            {name: 'Modular Memories', artist: 'Blusoul', label: 'Lost & Found', cue_time: "00:44:51"},
+            {name: 'Vapourspace (Intro Mix)', artist: 'Sasha', label: 'Last Night On Earth', cue_time: "00:50:40"},
+            {name: 'Nirvana', artist: 'Guy J', label: 'Bedrock', cue_time: "00:52:00"}
         ]
     },
 
@@ -25,20 +25,46 @@ $tracklists = [
         name: 'Live at Stella Polaris Frederiskberg, Denmark',
         date_played: '2021-8-7',
         artist: 'Guy J',
-        youtube_url: 'https://www.youtube.com/embed/9yDkHaSi90k',
+        youtube_url: 'https://www.youtube.com/watch?v=VcidDaRC-O8',
         tracks: [
             {name: 'Aeons (Tom Day Remix)', artist: 'Ryan Davis', label: nil, cue_time: "00:00:00"},
             {name: 'Hole in the Sun (Mr. Herbert Quain 94 Sunrise Version)', artist: 'Ricciardi ft. Notquietsound', label: nil, cue_time: "00:03:00"},
-            {name: 'Being Me', artist: 'Melchior Sultana & Janelle Pulo', label: 'Being Me', cue_time: "00:06:00"},
-            {name: nil, artist: nil, label: nil, cue_time: "00:08:00"},
-            {name: 'Dry Bridge', artist: 'Clarinets', label: nil, cue_time: "00:11:00"},
-            {name: 'Kingdom Adesse Versions Remix', artist: 'Maribou State ft. North Downs', label: 'Counter (Ninja)', cue_time: "00:15:00"},
-            {name: 'The Nanananinai', artist: 'Holmar', label: 'Get Physical', cue_time: "00:20:00"},
-            {name: nil, artist: nil, label: nil, cue_time: "00:25:00"},
-            {name: 'Rose Rogue (Nightmares On Wax ReRub Edit)', artist: 'St. Germain', label: 'Parlophone', cue_time: "00:29:00"},
-            {name: 'Misty Road', artist: 'Andre Lodemann', label: 'Best Works', cue_time: "00:30:00"},
-            {name: nil, artist: nil, label: nil, cue_time: "00:35:00"},
-            {name: 'Aurora (AM Mix)', artist: 'Guy J', label: 'Lost & Found', cue_time: "00:40:00"}
+            {name: 'Being Me', artist: 'Melchior Sultana & Janelle Pulo', label: 'Being Me', cue_time: "00:07:30"},
+            {name: nil, artist: nil, label: nil, cue_time: "00:13:00"},
+            {name: 'Dry Bridge', artist: 'Clarinets', label: nil, cue_time: "00:18:00"},
+            {name: 'Kingdom Adesse Versions Remix', artist: 'Maribou State ft. North Downs', label: 'Counter (Ninja)', cue_time: "00:22:30"},
+            {name: 'The Nanananinai', artist: 'Holmar', label: 'Get Physical', cue_time: "00:27:00"},
+            {name: nil, artist: nil, label: nil, cue_time: "00:32:00"},
+            {name: 'Rose Rogue (Nightmares On Wax ReRub Edit)', artist: 'St. Germain', label: 'Parlophone', cue_time: "00:38:00"},
+            {name: 'Misty Road', artist: 'Andre Lodemann', label: 'Best Works', cue_time: "00:47:00"},
+            {name: nil, artist: nil, label: nil, cue_time: "00:54:00"},
+            {name: 'Aurora (AM Mix)', artist: 'Guy J', label: 'Lost & Found', cue_time: "01:00:00"}
+        ]
+    },
+
+    {
+        name: 'Lost & Found pres. Echos 009',
+        date_played: '2020-4-17',
+        artist: 'Khen',
+        youtube_url: 'https://www.youtube.com/watch?v=cJbR_OA1HAg&ab_channel=Lost%26Found%2FGuyJ',
+        tracks: [
+            {name: 'Echos Intro', artist: 'Khen', label: nil, cue_time: "00:00:00"},
+            {name: nil, artist: nil, label: nil, cue_time: "00:07:30"},
+            {name: 'A Blue Rose for Daniele', artist: 'Rigooni', label: 'The Soundgarden', cue_time: "00:15:00"},
+            {name: 'Out of a Dream', artist: 'Khen', label: 'Lost Miracle', cue_time: "00:22:00"},
+            {name: 'Malestrom', artist: 'Montori & Gespona', label: nil, cue_time: "00:29:30"},
+            {name: 'A New Life (Nico Szabo Remix)', artist: 'Alfoa', label: 'BC2', cue_time: "00:36:05"},
+            {name: 'What Happened To Me', artist: 'Roy Rosenfeld', label: 'Rumors', cue_time: "00:41:00"},
+            {name: 'I Wish', artist: 'Khen', label: nil, cue_time: "00:52:00"},
+            {name: 'Emergency Routine', artist: 'Khen', label: 'Microcastle', cue_time: "01:03:15"},
+            {name: 'Livia Bang The Drums', artist: 'Guy Gerber', label: nil, cue_time: "01:08:10"},
+            {name: 'Freak', artist: 'CamelPhat', label: 'Moon Harbour', cue_time: "01:14:00"},
+            {name: 'Some Little Secrets', artist: 'Khen', label: 'Warung', cue_time: "01:21:20"},
+            {name: 'Mamba', artist: 'Khen', label: 'Trybesof', cue_time: "01:26:30"},
+            {name: 'Rhombus', artist: 'M.Rojas', label: nil, cue_time: "01:31:10"},
+            {name: 'Red Sun', artist: 'Vitaly Shturm', label: 'Electronic Tree', cue_time: "01:38:10"},
+            {name: 'Epidemic', artist: 'Khen', label: 'Lost & Found', cue_time: "01:41:10"},
+            {name: 'Closing Doors', artist: 'Khen', label: 'Anjunadeep', cue_time: "01:47:00"}
         ]
     }
 ]
